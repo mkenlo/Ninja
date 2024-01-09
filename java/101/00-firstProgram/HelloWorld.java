d01cde03339e8416e723e6ca8aadd38b8d14b5fc
Melanie <mel.lontsi@gmail.com>
2024-01-09 13:10:48 -0800

    back to basics:first java program

---
+++ java/101/00-firstProgram/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, it's me");
+        System.out.println("Just for my light to shine in this world");
+    }
+}
