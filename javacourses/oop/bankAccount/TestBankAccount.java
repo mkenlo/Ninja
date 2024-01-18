1c03868dfe8be4c4a8906e106bd6bbf379b8a560
Melanie <mel.lontsi@gmail.com>
2024-01-17 16:40:34 -0800

    corrected a memory leak bug

--- javacourses/oop/bankAccount/TestBankAccount.java
+++ javacourses/oop/bankAccount/TestBankAccount.java
@@ -3,49 +3,48 @@ import java.util.Scanner;
 
 public class TestBankAccount {
 
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args) throws InterruptedException {
 
-        String[] accountType = {"checking", "saving"};
+        String[] accountType = { "checking", "saving" };
         ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
         int numAccount = 3;
         Scanner reader = new Scanner(System.in);
 
         System.out.println("************** Welcome to the Bank ******************");
 
-        for(int i=0;i<numAccount;i++){
+        for (int i = 0; i < numAccount; i++) {
             BankAccount acc = new BankAccount();
-            System.out.println("... Opening account "+ (i+1)+": Account Num="+acc.getAccountNumber());
+            System.out.println("... Opening account " + (i + 1) + ": Account Num=" + acc.getAccountNumber());
             accounts.add(acc);
         }
-        System.out.println("Congratulations, you have opened "+BankAccount.getAccounts()+" accounts");
+        System.out.println("Congratulations, you have opened " + BankAccount.getAccounts() + " accounts");
         Thread.sleep(3000);
         System.out.print(">> What do you want to do next? type 'D' for deposit, 'W' for withdrawal or 'Q' to quit:  ");
-        String command  = reader.next();
-        while(!command.equalsIgnoreCase("q")){
+        String command = reader.next();
+        while (!command.equalsIgnoreCase("q")) {
 
-            System.out.print("Enter Account Id (1 to "+numAccount+"): ");
+            System.out.print("Enter Account Id (1 to " + numAccount + "): ");
             int accountId = reader.nextInt();
 
             System.out.print("Enter Account type (0 for checking, 1 for saving): ");
             int typeId = reader.nextInt();
 
-            String type  = accountType[typeId];
+            String type = accountType[typeId];
 
-            if(command.equalsIgnoreCase("d")){
+            if (command.equalsIgnoreCase("d")) {
                 System.out.print("How much do you want to deposit?: ");
                 double amount = reader.nextDouble();
-                accounts.get(accountId-1).deposit(type, amount);
+                accounts.get(accountId - 1).deposit(type, amount);
 
             } else if (command.equalsIgnoreCase("w")) {
                 System.out.print("How much do you want to withdraw?: ");
                 double amount = reader.nextDouble();
-                try{
-                    accounts.get(accountId-1).withdraw(type, amount);
-                }
-                catch (Exception e){
+                try {
+                    accounts.get(accountId - 1).withdraw(type, amount);
+                } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
-            }else{
+            } else {
                 System.out.println("Unknown command");
                 break;
             }
@@ -56,11 +55,14 @@ public class TestBankAccount {
                 System.out.println(account.getBalance());
                 System.out.println("......................");
             }
-            System.out.println("Total Money: "+BankAccount.getTotalMoney());
+            System.out.println("Total Money: " + BankAccount.getTotalMoney());
 
-            System.out.print(">> What do you want to do next? type 'D' for deposit, 'W' for withdrawal or 'Q' to quit:  ");
-            command  = reader.next();
+            System.out.print(
+                    ">> What do you want to do next? type 'D' for deposit, 'W' for withdrawal or 'Q' to quit:  ");
+            command = reader.next();
+
         }
+        reader.close();
 
     }
 }
